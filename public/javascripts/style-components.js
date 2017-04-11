951a1427202677eb43e9931e037892a63883e73b
144mdgross <144.mdgross@gmail.com>
2017-04-11 16:03:25 -0600

    begin basic styling. adding public javascripts.

--- public/javascripts/style-components.js
+++ public/javascripts/style-components.js
@@ -1,5 +1,20 @@
 $(document).ready(() => {
 
-$(".button-collapse").sideNav();
+  console.log('present');
 
- })
+  $(".button-collapse").sideNav();
+
+  $(document).ready(function() {
+      $('.carousel').carousel();
+  });
+
+  // Next slide
+  $('.carousel').carousel('next');
+  $('.carousel').carousel('next', 3); // Move next n times.
+  // Previous slide
+  $('.carousel').carousel('prev');
+  $('.carousel').carousel('prev', 4); // Move prev n times.
+  // Set to nth slide
+  $('.carousel').carousel('set', 4);
+
+})
