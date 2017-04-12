0d7b18ade9e1acf45605f1999d6f49c544771ceb
nicowave <geoidlife@gmail.com>
2017-04-11 22:18:53 -0600

    user_profile.hbs POST from form element led to trigger 404 configuring profile.js to get ready to POST new user

--- routes/profile.js
+++ routes/profile.js
@@ -8,8 +8,7 @@ router.get('/', function(req, res, next) {
 
   let personality = req.cookies.newUserPersonality.personality
       res.render('user_profile', { personality: personality })
-      // console.log('cookies', req.cookies);
-      // console.log(req.cookies.newUserPersonality);
+
 })
 
 
@@ -21,23 +20,37 @@ router.get('/:id', (req, res, next) => {
   // set a cookie with the user personality
   // this cookie also needs to say they are a new user
 
+
+
   // NOTE think about transition from first time user making profile to here and how info gets passed on. cookie?
   knex('users')
   .join('user_personality', 'users.id', 'user_personality.user_id')
   .where('users.id', id)
   .then(user => {
-    console.log(user);
+    // console.log(user);
     //not currently rendering age
       res.render('profile_view', {age: user[0].age, bio: user[0].bio, photos: user[0].photos, personality: user[0].personality})
   })
 
-// router.post('/', (req, res, next) => {
-//   knex('users')
-//   .insert {
-//     .returning(["id", "first_name", "last_name", "email", "hashed_password", "gender", "age", "bio", "usr_name", "photo"])
-//   }
-//   knex('user_personality')
-// })
+router.post('/', (req, res, next) => {
+
+  let name = (req.cookies.newUser.name).split('')
+  console.log(name);
+  // knex('users')
+  // .insert([{
+  //         "id":,
+  //         "first_name":,
+  //         "last_name":,
+  //         "email":,
+  //         "hashed_password":,
+  //         "gender":,
+  //         "age":,
+  //         "bio":,
+  //         "usr_name":,
+  //         "photo":
+  //       }], '*')
+  // knex('user_personality')
+})
 
 
   // PROBABLY DON'T NEED THIS
