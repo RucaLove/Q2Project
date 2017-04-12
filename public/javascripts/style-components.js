84562d10332b73ae8d1135358dfde48d94f87e18
sammzamm <sammzamm01@gmail.com>
2017-04-11 22:11:33 -0600

    logout buttons added

--- public/javascripts/style-components.js
+++ public/javascripts/style-components.js
@@ -17,4 +17,20 @@ $(document).ready(() => {
   // Set to nth slide
   $('.carousel').carousel('set', 4);
 
+
+      $('.modal').modal({
+          dismissible: true, // Modal can be dismissed by clicking outside of the modal
+          opacity: .5, // Opacity of modal background
+          inDuration: 300, // Transition in duration
+          outDuration: 200, // Transition out duration
+          startingTop: '4%', // Starting top style attribute
+          endingTop: '10%', // Ending top style attribute
+          ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+            console.log(modal, trigger);
+          },
+
+        }
+      );
+
+
 })
